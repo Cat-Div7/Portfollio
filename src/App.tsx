f1358c8cfe85a9679c870cd2f576083b3ae9415a
Cat-Div7 <omarashraf11234@gmail.com>
2026-03-14 22:11:11 +0200

    feat: add ScrollProgress component with spring animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import ScrollProgress from "./components/ScrollProgress";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ScrollProgress />
       <main>{/* sections go here */}</main>
     </>
   );
